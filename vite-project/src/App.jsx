8155381cb27be31d3f5fb3c3eb079531cfab9900
RAZAGHUL123 <92047284+RAZAGHUL123@users.noreply.github.com>
2023-09-25 19:17:19 -0700

    whoa

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Posts from './components/Posts';
 import Profile from './components/Profile';
 import LoginForm from './components/Login';
-import Register from './components/Register';
+import RegisterForm from './components/Register';
 
-function App() {
+function App() { 
   return (
     <Router>
       <NavBar />
@@ -16,7 +16,7 @@ function App() {
         <Route path="/posts" exact component={Posts} />
         <Route path="/profile" component={Profile} />
         <Route path="/login" component={LoginForm} />
-        <Route path="/register" component={Register} />
+        <Route path="/register" component={RegisterForm} />
         {/* Add more routes for other components */}
       
     </Router>
